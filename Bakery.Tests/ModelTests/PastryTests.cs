c819a8371e2484a6ddcd9969ec9e603af1e6015d
workmanmcr <workmanmcr@gmail.com>
2023-07-20 19:10:51 -0700

    1st pastry test passed

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -3,3 +3,19 @@ using Bakery.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+
+    [TestMethod]
+    public void PastryPromo_CalculateNumberOfFreePastries_FreePastry()
+    {
+    Pastry newPastryItem = new Pastry(11);
+
+    Assert.AreEqual(3, newPastryItem.FreePastry(11));
+    }
+  }
+}
